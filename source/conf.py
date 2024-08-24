a66c5f052be3a9ed6e69e53ab69d441c9b5b7d1b
An Pham <anpham6@gmail.com>
2024-08-24 15:50:13 -0700

    Publish: android-docs @ 0.2.1 [squared 5.3.2 / 0.15.2]

--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,7 @@
 project = 'squared'
 copyright = 'squared 2024'
 author = 'An Pham'
-release = '5.3.0'
+release = '5.3.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
